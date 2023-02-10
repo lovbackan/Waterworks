ed0fd71a0a4dd899d3fc229bff87ab3067049798
alun0511 <78014508+alun0511@users.noreply.github.com>
2023-02-10 13:47:31 +0100

    Fixed api-variable
    Added .env + .env.example
    changed path of fetch in apiData.js

--- src/components/ApiData.js
+++ src/components/ApiData.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 function ApiData() {
   function fetchData() {
     fetch(
-      "http://data.goteborg.se/RiverService/v1.1/MeasureSites/dfe7ae1d-a762-4356-9a7b-e70a4e7e4574?format=Json"
+      `https://data.goteborg.se/RiverService/v1.1/MeasureSites/${process.env.REACT_APP_APIKEY}?format=Json`
     )
       .then((response) => response.json())
       .then((data) => {
