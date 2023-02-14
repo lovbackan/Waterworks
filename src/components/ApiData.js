4d5c570c0389331cbc3231a096cba88f4308e5c9
MagnusVV <svartkub@msn.com>
2023-02-14 16:11:12 +0100

    Barebone React layout added.
    The graphics functions are now rendering, and the API-consumer works alongside it.

--- src/components/ApiData.js
+++ src/components/ApiData.js
@@ -1,14 +1,11 @@
 import { useEffect } from "react";
 
-
 //For getting Level from single location
 // `https://data.goteborg.se/RiverService/v1.1/Measurements/${process.env.REACT_APP_APIKEY}/MolndalCentrum/Level/2020-04-22/2020-05-31?format=Json`
 //Level or Flow
 
- //For single location now
+//For single location now
 // `https://data.goteborg.se/RiverService/v1.1/MeasureSites/${process.env.REACT_APP_APIKEY}/MolndalCentrum?format=Json`
-
-
 
 function ApiData() {
   let selectedLocations = [5, 4, 7];
@@ -35,7 +32,7 @@ function ApiData() {
     getLocations();
   });
 
-  return <p>Hello World!</p>;
+  /*   return <p>Hello World!</p>; */
 }
 
 export default ApiData;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../Header.css";
+import Title from "./Title.js";
+
+function Header() {
+  return (
+    <div className="Header">
+      <Title />
+    </div>
+  );
+}
+
+export default Header;
