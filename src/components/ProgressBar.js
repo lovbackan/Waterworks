77e33b246b5a949ad9e1ac949a7f87197367ee33
lovbackan <simon@lovbacka.se>
2023-02-20 15:54:59 +0100

    Progress with progressbar
    Almost done with the prgoressbar shell, have not implemented the datepicker and the checkbox functionality

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Circle from "./Circle";
+import "./Progress.css";
+
+const ProgressBar = () => {
+  const [circle] = useState(4);
+  const [active, setActive] = useState(0);
+  const [width, setWidth] = useState(0);
+  // något är fel här
+  useEffect(() => {
+    setWidth((100 / (circle - 1)) * active);
+  }, [circle.active]);
+
+  console.log(active);
+  const arr = [];
+  for (let i = 0; i < circle; i++) {
+    arr.push(
+      <Circle classname={i <= active ? "circle active" : "circle"} key={i}>
+        {i}
+      </Circle>
+    );
+  }
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="progressbar">
+          <div className="progress" style={{ width: width + "%" }}></div>
+          {arr}
+        </div>
+        <div className="button">
+          <button
+            className="prev btn"
+            disabled={active > 0 ? false : true}
+            onClick={() => {
+              active <= 0 ? setActive(0) : setActive(active - 1);
+            }}
+          >
+            Prev
+          </button>
+          <button
+            className="next btn"
+            disabled={active >= circle - 1 ? true : false}
+            onClick={() => {
+              active >= circle ? setActive(circle) : setActive(active + 1);
+            }}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
