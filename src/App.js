98299b21177df4f6c7ac8f8cc82261f2f33ed88b
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-02-20 12:56:45 +0100

    add useState for chart data

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import ApiData from "./components/ApiData";
 import Header from "./components/Header";
 import Chart from "./components/Chart";
 import Main from "./components/Main";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  console.log(data);
   return (
     <div className="App">
       <Header />
-      <ApiData />
-      <Chart />
+      <ApiData setData={setData} />
+      <Chart data={data}/>
       <Main />
     </div> /* ,
     (<ApiData />) , (<Chart />) */
--- src/components/ApiData.js
+++ src/components/ApiData.js
@@ -1,12 +1,12 @@
 import { wait } from "@testing-library/user-event/dist/utils";
 import { useEffect } from "react";
+import { useState } from "react";
 
-function ApiData() {
-  
+function ApiData({setData}) {
+
   let selectedStations = [5, 4, 1]; //Only for testing purposes. 
   let stations = []; //Contains all available stations after fetch.
- 
-
+  
   //Returns promise with 2dArray[station][day]
   function getSelectedStationsData(stationIDs, startDate, endDate) //StationIDs = array of IDs, Dates = YYYY-MM-DD
   { 
@@ -21,7 +21,7 @@ function ApiData() {
   //Returns array of all available stations
   function getStations() {
     return new Promise((resolve) => {
-      fetch(
+    fetch(
       `https://data.goteborg.se/RiverService/v1.1/MeasureSites/${process.env.REACT_APP_APIKEY}?format=Json`
     )
       .then((response) => response.json())
@@ -39,15 +39,15 @@ function ApiData() {
       stations = result;
       console.log(stations);
       getSelectedStationsData(selectedStations, "2022-05-05", "2022-06-05")
-      .then((results) => console.log(results));
+      .then((results) => setData(results));
     })
-  });
+  },[]);
 
   //Dates in format YYYY-MM-DD. Returns array where each element = one day
   function getStationData(station, startDate, endDate)
   {
     return new Promise((resolve) => {
-      fetch(
+       fetch(
         `https://data.goteborg.se/RiverService/v1.1/Measurements/${process.env.REACT_APP_APIKEY}/${station}/Level/${startDate}/${endDate}?format=Json`
       )
       .then((response) => response.json())
